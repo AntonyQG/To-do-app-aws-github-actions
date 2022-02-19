c7d2f278eb5eb3e12743642736f5f2c8eb241688
Antony Quintero <antonyquinterog@gmail.com>
2022-02-19 16:18:18 -0600

    Adding Task Bar and delete functionality with some styles

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,51 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view />
+    <router-view /> -->
+    <h1>Vue Todo App</h1>
+    <div class="add-task-wrapper">
+      <input type="text" v-model="newTaskInput" @keydown.enter="addTask" />
+      <button @click="addTask">Add Task</button>
+    </div>
+    <div class="task" v-for="task in tasks" :key="task.id">
+      <span>{{ task.name }}</span>
+      <span class="delete">
+        <button @click="removeTask(task.id)">X</button>
+      </span>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      newTaskInput: "",
+      tasks: [],
+    };
+  },
+  methods: {
+    addTask() {
+      let taskId = Math.floor(
+        Math.random() * (999999999 - 100000000) + 100000000
+      );
+      let newTask = {
+        id: taskId,
+        name: this.newTaskInput,
+      };
+      this.tasks.push(newTask);
+      this.newTaskInput = "";
+    },
+    removeTask(taskId) {
+      this.tasks = this.tasks.filter((task) => task.id !== taskId);
+    },
+  },
+};
+</script>
 
 <style lang="scss">
 #app {
@@ -15,6 +54,35 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  margin-top: 60px;
+  max-width: 500px;
+  margin: 0 auto;
+}
+
+button,
+input {
+  border-radius: 5px;
+  padding: 5px 10 px;
+  border: 1px solid#aaa;
+  margin: 5px;
+}
+
+.add-task-wrapper {
+  display: flex;
+}
+
+.add-task-wrapper input {
+  flex: 1;
+}
+
+.task {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #eee;
+  border-radius: 5px;
+  margin: 5px 10px;
+  padding: 5px 10px;
 }
 
 #nav {
